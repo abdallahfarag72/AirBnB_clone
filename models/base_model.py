136c4512b0a9f24e42608f50ae4fed2e8716fba9
Youssef Morad <youssefmohammed747@gmail.com>
2023-07-10 20:46:02 +0200

    inital baseModel (missing tests)

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+base_model module, it has class BaseModel
+which  defines all common attributes/methods for other classes
+"""
+from datetime import datetime
+import uuid
+
+class BaseModel:
+    """
+    BaseModel that defines all common attributes/methods for other classes
+    and handles serialization, deserialization
+    """
+    def __init__(self):
+        """ constructor initalize with id, create/update time
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ str representation of object instance
+        """
+        return f"{self.__name__} ({self.id}) {self.__dict__()}"
+
+    def to_dict(self):
+        """ turn object into dictionary
+        """
+        dic = self.__dict__
+        result_dic = {'__class__' : self.__name__}
+        for key, val in dic.items():
+            result_dic[key] = val
+        result_dic['created_at'] = result_dic['created_at'].isoformat()
+        result_dic['updated_at'] = result_dic['updated_at'].isoformat()
+        return result_dic
+    
