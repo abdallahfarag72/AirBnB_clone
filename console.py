6cad4d9013b9235f1b31eab8c5274129f1785d8a
Youssef Morad <youssefmohammed747@gmail.com>
2023-07-16 00:04:31 +0200

    console create and show command done

--- console.py
+++ console.py
@@ -2,6 +2,11 @@
 """The entry point of the command interpreter"""
 import cmd
 
+from models.base_model import BaseModel
+from models import storage
+
+all_classes = {'BaseModel': BaseModel}
+
 
 class HBNBCommand(cmd.Cmd):
     """Implementing the command interpreter functionality"""
@@ -13,7 +18,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """Implements EOF functionality"""
-        # print("")
         return True
 
     def do_help(self, arg):
@@ -23,6 +27,34 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Called when an empty line is entered"""
         pass
+
+    def do_create(self, arg):
+        """Create instance of specific class
+        """
+        if arg is None or arg.strip() == "":
+            print("** class name missing **")
+        elif arg not in all_classes.keys():
+            print("** class doesn't exist **")
+        else:
+            obj = all_classes[arg]()
+            obj.save()
+            print(obj.id)
+    
+    def do_show(self, arg):
+        """Show object info given its class and id
+        """
+        lst = arg.strip().split()
+        if len(lst) == 0:
+            print("** class name missing **")
+        elif lst[0] not in all_classes.keys():
+            print("** class doesn't exist **")
+        elif len(lst) == 1:
+            print("** instance id missing **")
+        elif f"{lst[0]}.{lst[1]}" not in storage.all():
+            print("** no instance found **")
+        else:
+            print(storage.all()[f"{lst[0]}.{lst[1]}"])
+
 
 
 if __name__ == "__main__":
