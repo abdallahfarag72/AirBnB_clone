70fd5fd934b78920cbc55f77ca1c15472772fcea
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-07-15 15:47:02 -0700

    created state class

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the State class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Defines a state and inherits from BaseModel
+    """
+    name = ""
