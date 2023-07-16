be6804d62c1905f63113445dd2c0e0f77f721d40
Youssef Morad <youssefmohammed747@gmail.com>
2023-07-16 23:57:20 +0200

    added simple user tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,4 +6,8 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
     def test_user(self):
-        pass
+        user1 = User()
+        self.assertEqual(type(user1.email), str)
+        self.assertEqual(type(user1.password), str)
+        self.assertEqual(type(user1.first_name), str)
+        self.assertEqual(type(user1.last_name), str)
